04029a9439f86810f5e55ab92e6de73647ee7369
David <da.b@hotmail.es>
2024-05-08 19:30:44 +0200

    Primer commit DAW05

---
+++ practica-martinez.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Script DAW - Actividad 5 - David Martínez de la Torre
+ *
+ * Este script contiene ejemplos de funciones documentadas.
+ *
+ * @package Practica_Documentacion
+ * @version 1.0
+ * @author David Martínez de la Torre
+ * @link http://www.dabmartinez.com
+ */
+
+/**
+ * Función para sumar dos números.
+ *
+ * Esta función toma dos números como entrada y devuelve su suma.
+ *
+ * @param int $num1 El primer número a sumar.
+ * @param int $num2 El segundo número a sumar.
+ * @return int La suma de $num1 y $num2.
+ * @since 1.0
+ */
+function sumar($num1, $num2) {
+    return $num1 + $num2;
+}
+
+/**
+ * Función para multiplicar dos números.
+ *
+ * Esta función toma dos números como entrada y devuelve su producto.
+ *
+ * @param int $num1 El primer número a multiplicar.
+ * @param int $num2 El segundo número a multiplicar.
+ * @return int El producto de $num1 y $num2.
+ * @since 1.0
+ */
+function multiplicar($num1, $num2) {
+    return $num1 * $num2;
+}
+
+// Esta anotación es solo para desarrolladores y no se mostrará en la documentación final.
+// @internal Esta función está destinada a ser utilizada solo por desarrolladores internos.
